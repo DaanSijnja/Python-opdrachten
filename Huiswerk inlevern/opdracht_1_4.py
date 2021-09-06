c018421e289d7787a65b518a55428769f688e961
DaanSijnja <68638963+DaanSijnja@users.noreply.github.com>
2021-09-06 18:34:46 +0200

    Update opdracht_1_4.py
    -check dat de einddatum niet kleiner is dan de start datum

--- Huiswerk inlevern/opdracht_1_4.py
+++ Huiswerk inlevern/opdracht_1_4.py
@@ -4,7 +4,7 @@ import requests
 
 def jaar():
     '''Vraag om het jaartal'''
-    var = int(input('Voer het jaar in :'))
+    var = int(input('Voer het jaar in YYYY:'))
     if(var < 1998 or var > 2021):
         #Er mag een jaartal tussen 1998 en 2021 ingevoerd worden
         print('Ongeldig jaar. Voer een getal tussen 1998 en 2021 in')
@@ -15,7 +15,7 @@ def jaar():
 
 def maand():
     '''Vraag om de maand'''
-    var = int(input('Voer het maand in :'))
+    var = int(input('Voer het maand in MM:'))
     if(var < 1 or var > 12):
         #Er mag een getal tussen 1 en 12 ingevoerd worden
         print('Ongeldige maand. Voer een getal tussen 1 en 12 in')
@@ -26,7 +26,7 @@ def maand():
 
 def dag(maand):
     '''Vraag om de dag'''
-    var = int(input('Voer het dag in :'))
+    var = int(input('Voer het dag in DD:'))
     if((maand == 2) and (var < 1 or var > 28)):
         #als de maand februari is mag de dag niet hoger zijn dan 28
         print('Ongeldige dag. Voer een dag tussen 1 en 28 in')
@@ -51,18 +51,43 @@ start_datum = [0,0,0]
 end_datum = [0,0,0]
 
 
+print('Voer de start datum in:')
+#input start datum
 start_datum[0] = jaar()
 start_datum[1] = maand()
 start_datum[2] = dag(start_datum[1])
 
+#input eind datum
+
+print('Voer de eind datum in:')
+#input start datum
+end_datum[0] = jaar()
+
+while(end_datum[0] < start_datum[0]):
+    #eind jaar mag niet kleiner zijn dan de start jaar
+    print('eind jaar mag niet kleiner zijn dan het start jaar')
+    end_datum[0] = jaar()
+
+end_datum[1] = maand()
+
+if(end_datum[0] == start_datum[0]):
+    #check of het eind jaar het zelfde is al het start jaar
+    while(end_datum[1] < start_datum[1]):
+        #eind maand mag niet kleiner zijn dan de start maand
+        print('eind maand mag niet kleiner zijn dan de start maand')
+        end_datum[1] = maand()
+
+end_datum[2] = dag(start_datum[1])
+
+if(end_datum[0] == start_datum[0] and end_datum[1] == end_datum[1]):
+    #check of het eind jaar het zelfde is al het start jaar en of de eind maand het zelfde is als de start maand
+    while(end_datum[2] < start_datum[2]):
+        #eind dag mag niet kleiner zijn als de start dag
+        print('eind dag mag niet kleiner zijn dan de start dag')
+        end_datum[2] = dag(start_datum[1])
 
 
-
-print(start_datum)
-
-
-
-
+print(start_datum,end_datum)
 
 
 '''
