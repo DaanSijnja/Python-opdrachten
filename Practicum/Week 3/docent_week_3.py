be362bae08217cbb4f9df792900581632c0dd3dd
DaanSijnja <68638963+DaanSijnja@users.noreply.github.com>
2021-12-03 08:26:14 +0100

    Week 3 practicum

---
+++ Practicum/Week 3/docent_week_3.py
@@ -0,0 +1,19 @@
+#import numpy as np
+import cv2
+
+# Lees een foto in (grijswaarden)
+img = cv2.imread('Kroatie_grijs.jpg', 0)
+#mg = cv2.resize(img, (800, 600)) 
+
+rows,cols = img.shape[:2]
+
+for i in range(rows):
+    for j in range(cols):
+        k = img[i,j]
+        img[i,j] = (k // 32) * 32
+ 
+cv2.imshow('plaatje',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+#cv2.imwrite('Kroatie_out.jpg', img)
--- Vison/live_coord.py
+++ Vison/live_coord.py
@@ -1,6 +1,15 @@
 import cv2 as cv
 import numpy as np
 
+blokjes = {
+	"wit" : [(19,13,227),(38,47,255)],
+	"blauw" : [(79,47,0),(115,255,88)],
+	"geel" : [(9,108,190),(50,255,255)],
+	"paars": [(147,0,32),(168,154,98)],
+	"oranje":[(1,104,185),(15,255,255)]
+}
+
+
 def find_aruco(image,search_id = None,dict= cv.aruco.DICT_4X4_250):
 	dictionary = cv.aruco.Dictionary_get(dict)
 	parameters =  cv.aruco.DetectorParameters_create()
@@ -77,13 +86,34 @@ def find_contours(image):
 	contours = contours[0] if len(contours) == 2 else contours[1]
 	return contours
 
-def find_by_color(image,min_range,max_range):
-	hsv = cv.cvtColor(image, cv.COLOR_RGB2HSV)
+def find_by_color(image,min_range,max_range,name):
+	hsv = cv.cvtColor(image, cv.COLOR_BGR2HSV)
 	mask = cv.inRange(hsv, min_range, max_range)
-	cv.imshow('screen', mask)
+	img_blur = cv.GaussianBlur(mask, (5, 5), 0)
+	cnts = cv.findContours(img_blur,cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+	cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+
+	for c in cnts:
+		area = cv.contourArea(c)
+		if(area > 1100):
+			x, y, w, h  = cv.boundingRect(c)
+			M = cv.moments(c)
+			cx = int(M['m10']/M['m00']) # middepunt
+			cy = int(M['m01']/M['m00'])	# middepunt
+			cv.circle(image,(cx,cy),2,(255,0,0),5)
+			#hull = cv.convexHull(c)
+			#epsilon = 0.1*cv.arcLength(hull,True)
+			#approx = cv.approxPolyDP(hull,epsilon,True)
+			#cv.drawContours(image,approx,-1,(255,255,255),10)
+			#cv.rectangle(image,(x, y), (x + w, y + h), (36,255,12), 2)
+			cv.putText(image, name, (cx,cy-5), cv.FONT_HERSHEY_PLAIN, 1, (0, 0, 255))
+			cv.putText(image,'x: ' +  str(cx) + ' y: ' + str(cy) , (cx,cy+20), cv.FONT_HERSHEY_PLAIN, 1, (0, 0, 255))
 
 
-cap = cv.VideoCapture(0)
+	cv.imshow('screen', img_blur)
+
+
+cap = cv.VideoCapture(0,cv.CAP_DSHOW)
 
 cap.set(cv.CAP_PROP_FRAME_WIDTH, 1080)
 cap.set(cv.CAP_PROP_FRAME_HEIGHT, 810)
@@ -100,8 +130,10 @@ while 1:
 	img_gray = imgout
 	if(len(found_markers[0]) >= 4):
 		imgout = draw_aumgented(found_markers[0],found_markers[1],img)
-		find_by_color(imgout,np.array([170, 10, 0]),np.array([255, 40, 255]))
-		
+		find_by_color(imgout,blokjes["blauw"][0],blokjes["blauw"][1],"Blauw")
+		find_by_color(imgout,blokjes["geel"][0],blokjes["geel"][1],"Geel")
+		find_by_color(imgout,blokjes["paars"][0],blokjes["paars"][1],"Paars")
+		find_by_color(imgout,blokjes["oranje"][0],blokjes["oranje"][1],"Oranje")
 		cv.imshow('out',imgout)
 		
 
